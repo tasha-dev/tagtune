5b2da463c5350ae14a32978239ee8aa49bc40875
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-20 14:49:47 +0330

    forgotted to statge this file in last commit

--- store/files.ts
+++ store/files.ts
@@ -1,41 +1,34 @@
 import { FilesStoreType, FileStoreMediaType } from "@/type/store";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
-const filesStore = create<FilesStoreType>()(
-  persist(
-    (set, get) => ({
+const filesStore = create<FilesStoreType>((set, get) => ({
+  files: [],
+  addFilesArray: (files) => {
+    set((state) => ({
+      files: [
+        ...state.files,
+        ...files.map(
+          (item) =>
+            Object.assign(item, {
+              id: crypto.randomUUID(),
+            }) as FileStoreMediaType,
+        ),
+      ],
+    }));
+  },
+  removeFile: (id) => {
+    const filesCopy = [...get().files];
+    const filesToSet = filesCopy.filter((item) => item.id !== id);
+
+    set({
+      files: filesToSet,
+    });
+  },
+  clearFiles: () => {
+    set({
       files: [],
-      addFilesArray: (files) => {
-        set((state) => ({
-          files: [
-            ...state.files,
-            ...files.map(
-              (item) =>
-                Object.assign(item, {
-                  id: crypto.randomUUID(),
-                }) as FileStoreMediaType,
-            ),
-          ],
-        }));
-      },
-      removeFile: (id) => {
-        const filesCopy = [...get().files];
-        const filesToSet = filesCopy.filter((item) => item.id !== id);
-        set({
-          files: filesToSet,
-        });
-      },
-      clearFiles: () => {
-        set({
-          files: [],
-        });
-      },
-    }),
-    {
-      name: "files-store", // localStorage key
-    },
-  ),
-);
+    });
+  },
+}));
 
 export default filesStore;
